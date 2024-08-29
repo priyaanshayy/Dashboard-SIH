b8004700fa163d8fa824124e78d59c14fb6953e0
priyaanshayy <priyanshayyy@gmail.com>
2024-08-30 00:28:38 +0530

    update

--- src/views/utilities/Shadow.js
+++ src/views/utilities/Shadow.js
@@ -1,19 +1,28 @@
-// src/pages/TypographyPage.js
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Grid, useTheme } from '@mui/material';
 import PageContainer from 'src/components/container/PageContainer';
 import StudentsHeadingCard from 'src/components/shared/StudentsHeadingCard';
 import DashboardCard from 'src/components/shared/DashboardCard';
 
 const data = [
   { id: 1, name: 'Priyanshay', age: 28, email: 'priyanshi@example.com', batch: 2025, image: 'https://imgv3.fotor.com/images/blog-richtext-image/a-woman-in-black-suit.jpg' },
-  { id: 2, name: 'Shruti', age: 34, email: 'shruti@example.com',  batch: 2026, image: 'https://imgv3.fotor.com/images/blog-cover-image/ID-Photo-Requirements-for-Passport-and-Identity-Card.jpg' },
-  { id: 3, name: 'Joe', age: 45, email: 'joe@example.com',  batch: 2027, image: 'https://www.shutterstock.com/image-photo/passport-photo-portrait-young-man-260nw-2437772333.jpg' },
+  { id: 2, name: 'Shruti', age: 34, email: 'shruti@example.com', batch: 2026, image: 'https://imgv3.fotor.com/images/blog-cover-image/ID-Photo-Requirements-for-Passport-and-Identity-Card.jpg' },
+  { id: 3, name: 'Joe', age: 45, email: 'joe@example.com', batch: 2027, image: 'https://www.shutterstock.com/image-photo/passport-photo-portrait-young-man-260nw-2437772333.jpg' },
 ];
 
-const colors = ['#bbdefb', '#c8e6c9', '#ffecb3', '#d7ccc8', '#e0f2f1', '#ffcdd2', '#d1c4e9'];
-
 const TypographyPage = () => {
+  const theme = useTheme(); // Get the theme object
+
+  const colors = [
+    theme.palette.primary.light,
+    theme.palette.secondary.light,
+    theme.palette.info.light,
+    theme.palette.success.light,
+    theme.palette.warning.light,
+    theme.palette.error.light,
+    theme.palette.grey[200],
+  ];
+
   return (
     <PageContainer title="Students" description="Students">
       <Grid container spacing={3}>
--- src/views/utilities/TypographyPage.js
+++ src/views/utilities/TypographyPage.js
@@ -1,6 +1,6 @@
 // src/pages/TypographyPage.js
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Grid,useTheme} from '@mui/material';
 import PageContainer from 'src/components/container/PageContainer';
 import HeadingCard from 'src/components/shared/HeadingCard';
 import DashboardCard from 'src/components/shared/DashboardCard';
@@ -11,9 +11,21 @@ const data = [
   { id: 3, name: 'Joe', age: 45, email: 'joe@example.com', company: 'Facebook', batch: 2022, image: 'https://www.shutterstock.com/image-photo/passport-photo-portrait-young-man-260nw-2437772333.jpg' },
 ];
 
-const colors = ['#bbdefb', '#c8e6c9', '#ffecb3', '#d7ccc8', '#e0f2f1', '#ffcdd2', '#d1c4e9'];
+// const colors = ['#bbdefb', '#c8e6c9', '#ffecb3', '#d7ccc8', '#e0f2f1', '#ffcdd2', '#d1c4e9'];
 
 const TypographyPage = () => {
+  const theme = useTheme(); // Get the theme object
+
+  const colors = [
+    theme.palette.primary.light,
+    theme.palette.secondary.light,
+    theme.palette.info.light,
+    theme.palette.success.light,
+    theme.palette.warning.light,
+    theme.palette.error.light,
+    theme.palette.grey[200],
+  ];
+
   return (
     <PageContainer title="Alumni" description="Alumni">
       <Grid container spacing={3}>
