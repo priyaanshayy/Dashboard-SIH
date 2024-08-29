3018d95714e2414af6be49654a4b0b8b40c08768
priyaanshayy <priyanshayyy@gmail.com>
2024-08-30 01:19:07 +0530

    update

--- src/layouts/full/header/data.js
+++ src/layouts/full/header/data.js
@@ -3,11 +3,9 @@ import img1 from 'src/assets/images/profile/user-1.jpg';
 import img2 from 'src/assets/images/profile/user-2.jpg';
 import img3 from 'src/assets/images/profile/user-3.jpg';
 import img4 from 'src/assets/images/profile/user-4.jpg';
-
 import icon1 from 'src/assets/images/svgs/icon-account.svg'
 import icon2 from 'src/assets/images/svgs/icon-inbox.svg'
 import icon3 from 'src/assets/images/svgs/icon-tasks.svg'
-
 import ddIcon1 from 'src/assets/images/svgs/icon-dd-chat.svg'
 import ddIcon2 from 'src/assets/images/svgs/icon-dd-cart.svg'
 import ddIcon3 from 'src/assets/images/svgs/icon-dd-invoice.svg'
